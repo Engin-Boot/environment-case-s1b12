37f0e62fc468150a8df0507ae27cb8b26dcf6083
Shivaprasad <shivaprasadsmys@gmail.com>
2020-11-13 13:18:48 +0530

    Updated shell scripts for receiver build

--- BASH/main_workflow.sh
+++ BASH/main_workflow.sh
@@ -1,10 +1,9 @@
 # Build sender-executable
 g++ -Wall -Wextra -Werror -o sender-executable sender.cpp SenderModule/CSVReader.cpp SenderModule/Timer.cpp Lib/DataProcessingUtility.cpp
 
-# Build receiver-executable
-g++ -Wall -Wextra -Werror -o receiver-executable receiver.cpp -pthread
+# # Build receiver-executable
+g++ receiver.cpp ReceiverModule/environment_checker.cpp ReceiverModule/Logger.cpp Lib/DataProcessingUtility.cpp -o receiver-executable -pthread
 
-
-# Run sender | receiver
+# # Run sender | receiver
 
 ./sender-executable | ./receiver-executable
