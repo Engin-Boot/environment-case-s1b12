64be048d066c8d45855146e54f6f7cce60042f4d
Shivaprasad <shivaprasadsmys@gmail.com>
2020-11-13 13:25:22 +0530

    updated static analysis shell script

--- BASH/static_analysis.sh
+++ BASH/static_analysis.sh
@@ -1,2 +1,3 @@
 # Static Analysis | Sender
 cppcheck --enable=all --error-exitcode=1 --suppress=missingIncludeSystem --suppress='*:Lib/catch*' SenderModule/CSVReader_test.cpp SenderModule/CSVReader.cpp SenderModule/Timer.cpp SenderModule/THsensor_test.cpp
+cppcheck --enable=all --error-exitcode=1 --suppress=missingIncludeSystem ReceiverModule/*.cpp receiver.cpp
